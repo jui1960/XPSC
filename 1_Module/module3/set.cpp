4841f3d32f12ee8049988f041d95ee2eac7d1eae
jui1960 <juifull196044@gmail.com>
2025-11-09 23:34:26 +0600

    set store unic value and sort

--- 1_Module/module3/set.cpp
+++ 1_Module/module3/set.cpp
@@ -4,5 +4,23 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout << "jui";
+    int n;
+    cin>>n;
+    set<int>s;
+    for(int i = 0; i<n; i++){
+        int x;
+        cin>>x;
+        s.insert(x);
+    }
+
+    auto it = s.begin();
+    cout<<*it<<'\n';
+
+    
+    //set short value rakhe...20 9 6 200 9 4 ,,
+    //akn eitar begin() ber korte chaile 4 ashbe
+
+
+
+    
 }
