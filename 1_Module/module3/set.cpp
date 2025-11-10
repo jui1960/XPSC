96fef7087e54b3c8220e193f104f12e21735ee97
jui1960 <juifull196044@gmail.com>
2025-11-10 23:50:23 +0600

    set

--- 1_Module/module3/set.cpp
+++ 1_Module/module3/set.cpp
@@ -5,22 +5,60 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n;
-    cin>>n;
-    set<int>s;
-    for(int i = 0; i<n; i++){
+    cin >> n;
+    set<int> s;
+    for (int i = 0; i < n; i++)
+    {
         int x;
-        cin>>x;
+        cin >> x;
         s.insert(x);
     }
 
-    auto it = s.begin();
-    cout<<*it<<'\n';
+    // auto it = s.begin();
+    // it++;
+    // it++;
+    // cout<<*it<<'\n';
 
+    s.erase(200);
+    for (auto value : s)
+    {
+        cout << value << " ";
+    }
+    cout << '\n';
+
+    cout << s.count(5) << '\n'; // jody 5 khuje na pai taile 0 print korbe otherwise 1
+
+    auto it = s.find(25);
+    if (it != s.end())
+    {
+        cout << "found" << '\n';
+    }
+    else
+        cout << "not found" << '\n';
+
+    // lower bound
+
+    // int y;
+    // cin >> y;
+    // auto itt = s.lower_bound(y);
+
+    // cout << *itt << '\n';
+
+
+
+    //uper bound
     
-    //set short value rakhe...20 9 6 200 9 4 ,,
-    //akn eitar begin() ber korte chaile 4 ashbe
+    int y;
+    cin >> y;
+    auto itt = s.upper_bound(y);
+    cout << *itt << '\n';
 
 
 
-    
+
+
+
+
+    // set short value rakhe...20 9 6 200 9 4 ,,
+    // akn eitar begin() ber korte chaile 4 ashbe
 }
