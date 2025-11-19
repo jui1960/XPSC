14be02cd548e27337a49c9051ae97b8d92ba3128
jui1960 <juifull196044@gmail.com>
2025-11-19 17:22:50 +0600

    simple code

---
+++ 1_Module/module3/Sets_STL.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int q;
+    cin >> q;
+    set<int> s;
+
+    while (q--)
+    {
+
+        int a, b;
+        cin >> a >> b;
+        if (a == 1)
+            s.insert(b);
+
+        // cout << s.size() << '\n';
+
+        else if (a == 2)
+        {
+            if (!s.empty())
+            {
+                s.erase(b);
+            }
+        }
+        else
+        {
+            auto it = s.find(b);
+            if (it!= s.end())
+            {
+                cout << "Yes" << '\n';
+            }
+            else
+                cout << "No" << '\n';
+        }
+    }
+
+    return 0;
+}
