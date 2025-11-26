721b729def7cf5f0f551ee9711eaa6e060b36a15
jui1960 <juifull196044@gmail.com>
2025-11-26 19:48:10 +0600

    add

--- 1_Module/module3/C_Train_and_Queries.cpp
+++ 1_Module/module3/C_Train_and_Queries.cpp
@@ -1,9 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, q;
+        cin >> n >> q;
+        map<int, set<int>> mp;
+        for (int i = 1; i <= n; i++)
+        {
+
+            int x;
+            cin >> x;
+            mp[x].insert(i);
+        }
+
+        // for (auto [x, y] : mp)
+        // {
+        //     cout << x << "-> ";
+        //     for (auto value : y)
+        //     {
+        //         cout << value << " ";
+        //     }
+        //     cout << '\n';
+        // }
+
+        for (int i = 1; i <= q; i++)
+        {
+            int l, r;
+            cin >> l >> r;
+            if ((mp.find(l) == mp.end()) || (mp.find(r) == mp.end()))
+            {
+                cout << "NO" << '\n';
+            }
+            else
+            {
+                int ans, res;
+                ans = *mp[l].begin();
+                res = *mp[r].rbegin();
+                if (ans < res)
+                {
+                    cout << "YES" << '\n';
+                }
+                else
+                    cout << "NO" << '\n';
+            }
+        }
+    }
+
     return 0;
 }
