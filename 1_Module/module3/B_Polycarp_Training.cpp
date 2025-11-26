85115804b3423a5adeb6e780315152811373c1ef
jui1960 <juifull196044@gmail.com>
2025-11-26 19:07:45 +0600

    problem

--- 1_Module/module3/B_Polycarp_Training.cpp
+++ 1_Module/module3/B_Polycarp_Training.cpp
@@ -1,9 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    int n;
+    cin >> n;
+    multiset<int> ml;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        ml.insert(x);
+    }
+    int ans = 0, prblm = 1;
+    while (!ml.empty())
+    {
+        auto lb = ml.lower_bound(prblm);
+        if (lb != ml.end())
+        {
+            ans++;
+            ml.erase(lb);
+        }
+        else
+        {
+            break;
+        }
+        prblm++;
+    }
+    cout<<ans<<'\n';
+
     return 0;
 }
